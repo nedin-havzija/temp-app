2be63435ce28dced560d5f7d902badbe6dcd9d7c
nedin-havzija <nedin.havzija@edu.tbz.ch>
2024-01-12 17:56:37 +0100

    created script for fetching info

---
+++ Bash_Script_Nedin/fetch_temp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+DATA_URL="http://kel.internet-box.ch:2280/log/data_temp.txt"
+LOCAL_FILE="data_temp.txt"
+
+while true; do
+    # Fetch the file from the server using curl
+    curl -s ${DATA_URL} > ${LOCAL_FILE}
+
+    if [[ -f ${LOCAL_FILE} ]]; then
+        # Extract the last temperature entry from the file
+        last_entry=$(tail -n 1 ${LOCAL_FILE})
+        
+        # Extract temperature value from the last entry
+        temperature=$(echo ${last_entry} | awk -F, '{print $2}')
+        
+        echo "Current temperature: ${temperature} °C"
+        
+        # Remove the temporary local copy
+        rm ${LOCAL_FILE}
+    else
+        echo "Error: Temperature data file not found on the server"
+    fi
+
+    # Sleep for 30 seconds
+    sleep 30
+done
+
